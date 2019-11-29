0c35e27a00415202fde15e4f758ffe5ae5656be0
AlmuHS <almuhs@github.com>
2019-11-29 09:40:41 +0100

    gen_trec_rel: written results to file

--- Practica1/parse_LISA.py
+++ Practica1/parse_LISA.py
@@ -301,6 +301,18 @@ def gen_trec_rel(in_file, out_file):
                     all_docs.append(word)
 
     
+        for ref in rel_docs:		
+
+            for doc in all_docs:
+                line = str(ref) + "\t" + "0" +"\t" + str(doc) + "\t"
+                if doc in rel_docs[ref]:
+                    line += "1"
+                else:
+                    line += "0"
+
+                output.write(line + "\n")                   
+
+
         print(rel_docs)
         print("\n\n")
         print(all_docs)
